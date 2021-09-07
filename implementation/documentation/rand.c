a7c1c962ca3d309fccf2650fbc0790ba816aafd9
likhithn1999 <likhithn.1si17ei021@gmail.com>
2021-09-07 18:30:58 +0530

    update

---
+++ implementation/documentation/rand.c
@@ -0,0 +1,18 @@
+/**
+ * @file rand.c
+ * @author your name (you@domain.com)
+ * @brief 
+ * @version 0.1
+ * @date 2021-09-07
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+int generateRandomNumber(int n)
+{
+    srand(time(NULL));
+    return rand() % n;
+}
